52fa735d476d9654fb5b20ee8113a0209b81ef0a
Lucas Vinicius <Lucasvinicius3105@gmail.com>
2020-05-13 09:58:47 -0300

    Usuario Lucas: primeiro commit...

---
+++ src/MazeCodePackage/MazeCode.java
@@ -0,0 +1,169 @@
+package MazeCodePackage;
+import java.util.Scanner;
+public class MazeCode {
+public static void main(String[] args) {
+	Scanner entrada = new Scanner (System.in);
+	
+	 String nickname;
+	 
+	 // fazer menu para entrar no jogo 
+	 System.out.print("Digite seu nome para iniciar o jogo: ");
+	 nickname = entrada.next();
+	 
+	 System.out.println("\n Em um lindo dia ensolarado," +nickname+" estava a caminho de sua casa, quando a última coisa que conseguiu ver foi um saco preto sendo colocado rapidamente em sua cabeça,"
+		+ " \nsentiu uma picada de agulha em seu braço esquerdo, que logo após o apagou.\n");
+	 
+	 
+	 System.out.println(" Algum tempo depois...");
+	 System.out.println(" Acordou assustado, estava em uma sala com uma pequena janela onde só podia ver muros pelo lado de fora,não sabia onde estava, \n"
+	 	+ "não se lembrava de nada do que tinha acontecido, nem mesmo de sua vida antiga, apenas de seu primeiro nome, "+nickname+".");
+	 
+	 
+	 System.out.println(" Desesperado, olhou por todos os lados da sala, onde conseguiu localizar apenas uma folha de papel com alguma mensagem, então correu em sua direção e a leu. \n "
+	 	+ "\n Olá "+nickname+", se está lendo esta mensagem é por que tudo está ocorrendo como planejado. Você foi o escolhido para desvendar o maior mistério já criado por nós.\n"
+	 	+ "O seu cérebro funciona de modo diferente dos demais, de modo extraordinário, que, se conseguir passar por todos os testes sem a ajuda de ninguém, \n"
+	 	+ "terá algo único que nenhum dinheiro do mundo possa comprar.\n Sei que está sofrendo uma onda de frustração neste momento, mas terá suas respostas assim que for avançando,"
+	 	+ "então dê o seu melhor neste desafio, e lembre-se, \n"
+	 	+ "Cruel é bom! ");
+	 
+	 
+	 System.out.println("\n  Depois de ler esta mensagem, ficou ciente de que havia uma jornada difícil a sua frente. Saiu da pequena sala e encontrou uma caixa com itens preparatórios,\n"
+	 	+ "como tênis de corrida, uma garrafa de água, e um grande mapa, que só de olhar já dava para entender que tudo aquilo fazia parte de um labirinto,\n"
+	 	+ "não queria ficar parado ali refletindo o que estava acontecendo, apenas queria sair daquele lugar o mais rápido possível, então se levantou, certificou-se do caminho \n"
+	 	+ "que deveria seguir no mapa e começou a correr. \n");
+	 		
+	 // primeira escolha de direcao
+	 
+	 System.out.print(" Depois de 40 minutos correndo, entrando em corredores atrás de corredores, e ouvindo barulhos esquisitos, ficou à frente de uma longa porta de puro aço, \n"
+		+ "onde estava escrito “TESTE 1”, e logo ao lado tinha uma caixinha com um aparelho semelhante à um ipad dentro, "+nickname+" o pegou e uma mensagem de áudio começou \n"
+		+ "a ser reproduzida “Estes testes consistem em 10 perguntas, cada uma com 4 alternativas, se acertar todas estará livre deste labirinto, senão, virará comida de verdugos, \n\n");
+	 
+	 
+	 // primeiro questionario"
+	 
+	System.out.println("“PRIMEIRA PERGUNTA”\n");
+	
+	
+	//continuacao da historia
+	
+	System.out.println("Acertando então a resposta, a porta à sua frente se abriu e "+nickname+" continuou avançando entre as altas paredes de aço e cimento");
+	
+	// segunda escolha de direcao
+	
+	System.out.println("Após caminhar por alguns minutos chegando então em uma porta totalmente idêntica à primeira, mas moldado neste estava escrito “TESTE 2”\n\n"
+			+ nickname +" pegou o aparelho ao lado e a pergunta apareceu na tela. \n");
+	
+	// segundo questionario
+	
+	System.out.println("“SEGUNDA PERGUNTA”.\n");
+	
+	//continuacao da historia
+	System.out.println("Continuou avançando, desta vez os barulhos esquisitos que vinha ouvindo no caminho parecia estar ficando cada vez mais próximos, o que o deixou um pouco aflito. \n");
+	
+	// terceira escolha de direcao
+	
+	System.out.println("Mas depois de correr cerca de 30 minutos chegou em um corredor sem saída, apenas com um círculo com cerca de 1 metro de diâmetro, nele escrito “TESTE 3”, \n"
+		+ "que poderia ser uma passagem, desta vez não havia nenhum aparelho para que ele pudesse responder ao teste 3, procurou em todos os cantos e não conseguiu encontrar absolutamente nada,\n"
+		+ "a única coisa que tinha notado é que em todas as paredes estava escrito “Cruel é bom!”, ele a leu em vós alta e automaticamente abriu apenas uma pequena parte do círculo, \n"
+		+ "revelando então a caixa com o aparelho. "
+		+ nickname + " o pegou e a pergunta apareceu. \n");
+	
+	// terceiro questionario
+	
+	System.out.println("“TERCEIRA PERGUNTA”.\n");
+	
+	//continuacao da historia
+	System.out.print(" Quando respondeu corretamente, o círculo se abriu por completo e ele se arrastou adentro até sair do outro lado. Tomou um pouco de água, respirou profundamente, \n"
+			+ "e começou a corrida novamente. \n");
+	
+	//quarta escolha de direcao
+	
+	System.out.print("O medo era notável em sua face, mas "+nickname+" não parava, continuava avançando a cada segundo que passava, então chegou em um corredor que havia 4 portas, \n"
+			+ "na parede estava pichado “TESTE 4”, sem entender, pegou o aparelho que estava entre as duas portas do meio e uma outra mensagem de voz apareceu “Parabéns "+nickname+",\n"
+			+ "estás indo bem por enquanto. Agora cada porta corresponde a uma das alternativas, apenas uma delas te dará a oportunidade de continuar avançando em sua jornada, \n"
+			+ "as outras três terá monstros que nunca foram derrotados antes, então se errar, considere-se morto.”. A vós se calou e a pergunta apareceu na tela. \n\n");
+	
+	// quarto questionario
+	System.out.println("“QUARTA PERGUNTA”.\n");
+			
+	// continuacao da historia
+		
+	System.out.println("Quando respondeu, as portas continuaram intactas, não entendia o porquê, mesmo se tivesse errado a resposta uma das portas abriria, então olhou novamente para \n"
+			+ "o aparelho e a mensagem de voz voltou “Ah, esquecemos de te avisar! Agora para cada passagem que terá que ser aberta, duas perguntas terão que ser respondidas, \n"
+			+ "se errar alguma, já sabe! Então a quinta pergunta apareceu na tela. \n ");
+			
+	//quinto questionario
+	
+	System.out.println("“QUINTA PERGUNTA” \n");
+			
+	//continuacao da historia
+			
+	System.out.println("Depois que as respondeu, na tela do aparelho apareceu “Respostas corretas”, então uma das portas se abriu e ele avançou pelo labirinto.\n");
+			
+	//quinta escolha de direcao
+	
+	System.out.println(" Sua velocidade já estava se reduzindo, "+nickname+" se sentia um pouco cansado, parou, olhou no mapa o caminho, \n"
+			+ "tomou um pouco de água e percebeu que suas memórias estavam voltando, conseguiu se lembrar da neve, da chuva e de sua casa, o que o deixou mais motivado,\n"
+			+ "então guardou o mapa, garrafa de água e partiu. /n");
+	System.out.println(nickname+" se viu dentro de um túnel com luzes um pouco fracas, tornando as coisas difíceis de serem vistas. Neste túnel havia outra passagem, /n"
+			+ "“TESTE 5” estava escrito na porta, e logo abaixo havia um botão. Ele pressionou este botão e então o aparelho saiu de dentro da porta, pronto para ser usado. \n"
+			+ nickname+" o pegou e a sexta pergunta apareceu. \n");
+	
+	//sexto questionario
+	System.out.println("“SEXTA PERGUNTA”. \n");
+	
+	System.out.println("Respondeu-a e a sétima apareceu logo em seguida. \n");
+	
+	//setimo questionario
+	System.out.println("“SÉTIMA PERGUNTA” \n");
+	
+	//continuacao da historia
+	System.out.print(" Quando "+nickname+" as respondeu, ficou se perguntando se tinha as acertado, e então a porta se abriu. "+nickname+" atravessou a porta e rapidamente \n"
+			+ "um flash de lembranças o atingiu em cheio, deixando-o atordoado e com uma dor de cabeça insuportável. Não sabia o que estava acontecendo. \n"
+			+ "Se deitou no chão e do nada veio uma vós que ecoava por todos os lados, era a voz de uma mulher, ela dizia, “Realmente está me surpreendendo, "+nickname+".\n"
+			+ " Das 114 pessoas que já tentaram, você foi o que mais chegou longe. Estou começando a acreditar que você possa conseguir. Se conseguir salvará a vida de tantas pessoas...”,\n"
+			+ "a vós cessou, "+nickname+" se levantou, sabia que sua memória estava voltando pouco a pouco, olhou para sua frente, e seguiu. \n");
+	
+	//sexta escolha de direcao
+	
+	System.out.println(" Após caminhar por muito tempo chegou. O lugar era maravilhoso, parecia uma pequena vila, tinha vegetações, uma fonte sem água no centro, estátuas que ele acreditava \n"
+			+ "que era de alguns deuses, e uma porta vermelho-sangue que era impossível não ser notada. Thomas correu até ela, tinha as marcações grifadas na porta “TESTE 6” e “Cruel é bom!”,\n"
+			+ "não sabia o que aquilo significava, mas pressionou o botão e, ao invés de sair um aparelho semelhante a um ipad, na própria porta apareceu um televisor com as perguntas oitava e nona. \n");
+	
+	////oitavo questionario
+	System.out.println("“OITAVA PERGUNTA” \n"); 
+	
+	// nono questionario
+	System.out.println("“NONA PERGUNTA” \n");
+	
+	//continuacao da historia
+	System.out.println(" Ao lado do televisor havia um pequeno microfone, tomas o colocou na orelha e ouviu. “Extraordinário!” Dizia a voz “Você já está prestes a sair deste labirinto, \n"
+			+ "apenas mais um acerto e você poderá ir para casa com todas as suas memórias restauradas. A última pergunta se encontra a 100 metros desta porta, acerte-a e venha até nós.\n"
+			+ " E jamais esqueça, Cruel é bom!” \n");
+	System.out.println("A porta se abriu e "+nickname+" estava à beira de um precipício, conseguia ver a última porta do outro lado,\n"
+			+ "mas para chegar até lá teria que atravessar uma ponte feita de corda e madeira, que parecia ter mil anos de idade. "+nickname+" suspirou, meio ofegante e deu o primeiro passo na ponte.\n"
+			+ " A madeira rugia, um vento forte o ricocheteava de lado, mas ele não parou, passo a passo ele conseguiu atravessar a ponte e se livrar do penhasco,\n"
+			+ "mas quando olhou para lugar de que tinha vindo, um enorme ser que parecia com uma aranha de 2 metros, com pernas de ferro e olhos vermelhos estava parada o olhando. \n"
+			+ " Em disparada ela começou a correr em direção a ele, rapidamente "+nickname+" teve a brilhante ideia de desamarrar as cordas da ponte para que o monstro caísse na escuridão do abismo. \n"
+			+ "Ele começou, desamarrou a primeira corda e a ponte tombou para o lado, mas isso não parecia ser um problema para a aranha, pois ela continuou avançando na mesma velocidade. \n"
+			+ "Quando faltava menos de dois metros para a aranha alcançar o outro lado, "+nickname+" soltou a outra corda e tanto a ponte quanto esse monstro horrendo despencaram para a morte. \n"
+			+ nickname+" estava completamente assustado, suava, mas se obrigou a manter o controle. Virou-se para a porta que era a única coisa que o separava de sua liberdade, \n"
+			+ "pegou o aparelho que estava ao lado e começou ler a décima pergunta.\n");
+	
+	// decimo questionario
+	System.out.println("“DÉCIMA PERGUNTA” \n");
+	
+	// continuacao da historia
+	System.out.println(nickname+" respondeu e a porta se abriu de imediato. Atrás da porta havia dezenas de pessoas o olhando como se ele tivesse vindo de outro planeta, "+nickname+" se sentiu desconfortável,\n"
+			+ "e então uma mulher com aparência de uns 40 anos se aproximou de "+nickname+" e disse para ele não se preocupar, pois daria todas as respostas para ele agora. \n"
+			+ "Ambos se sentaram em cadeiras confortáveis e ela começou. "+nickname+", você ficou dormindo por 12 anos desde que foi capturado por um de nossos agentes, no ano de 2063, que foi a 10 anos atrás,\n"
+			+ "houve uma forte epidemia que ficou conhecida como Covid-63, esta epidemia apodrecia o cérebro dos infectados em menos de 6 meses.\n"
+			+ " Você foi o escolhido por nós para concluir este teste porque sabíamos que se você o concluísse com sucesso, seu cérebro geraria uma substância que seria a chave principal para esta epidemia.\n"
+			+ " Nossa organização se chama Cruel, e nosso maior objetivo é fazer o melhor para a humanidade "+nickname+" sentiu muita raiva de tudo aquilo, mas no fim compreendeu que era para o bem da humanidade.\n"
+			+ " Retiraram a substancia do cérebro de "+nickname+" e ofereceu recursos para que ele pudesse viver bem e em segurança.\n"
+			+ "3 anos depois esta doença tinha sido extinta e a humanidade venceu um novo desafio.\n\n");
+	
+	//agradecimentos
+	System.out.println("OBRIGADO POR JOGAR");
+}
+}
